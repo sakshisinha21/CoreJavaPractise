6c36679e420506df133b2b3796b95c0dc6fdb4a9
sakshisinha21 <115772696+sakshisinha21@users.noreply.github.com>
2022-10-14 11:43:52 +0530

    Add files via upload

---
+++ 121022/PrintNameDetails.java
@@ -0,0 +1,7 @@
+class PrintNameDetails
+{
+	public static void main(String args[])
+	{
+		System.out.println("Name: "+ args[0]+"\n"+"Age: "+args[1]+"\n"+"Course: "+args[2]+"\n"+"Score: "+args[3]);
+	}
+}
